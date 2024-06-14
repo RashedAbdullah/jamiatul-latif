d7cd8852f5e54d4bf3deb5cf047bc35381138458
RashedAbdullah <maarashed40@gmail.com>
2024-06-14 19:21:11 +0600

    Trying to fix some errors

--- components/auth/signin-form.jsx
+++ components/auth/signin-form.jsx
@@ -1,43 +1,68 @@
+"use client";
+
+import { handleSignin } from "@/actions";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 import SubTitle from "../sub-title";
 
-const SigninForm = () => {
+const SigninForm = ({ lang = "bn" }) => {
+  const [error, setError] = useState();
+  const router = useRouter();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const formData = new FormData(event.currentTarget);
+      const response = await handleSignin(formData);
+      if (!!response.error) {
+        setError(response.error.message);
+      } else {
+        router.push(`/${lang}`);
+      }
+    } catch (err) {
+      setError(err.message);
+    }
+  };
   return (
-    <form>
-      <SubTitle showIcon={false}>সাইনইস করুন</SubTitle>
-      <div className="flex flex-col gap-5">
-        {/* ইমেইল */}
-        <div className="grid grid-cols-10">
-          <p className="col-span-2 text-end px-3">ইমেইল:</p>
-          <div className="col-span-8">
-            <input
-              className="w-full py-1 px-3 resize-none focus:outline outline-1 outline-green-500 border-b border-green-500 bg-transparent transition-all duration-300"
-              type="email"
-              placeholder="আপনার ইমেইল"
-              name="email"
-            />
+    <>
+      {error && <div className="text-center text-sm text-red-500">{error}</div>}
+      <form onSubmit={handleSubmit}>
+        <SubTitle showIcon={false}>সাইনইস করুন</SubTitle>
+        <div className="flex flex-col gap-5">
+          {/* ইমেইল */}
+          <div className="grid grid-cols-10">
+            <p className="col-span-2 text-end px-3">ইমেইল:</p>
+            <div className="col-span-8">
+              <input
+                className="w-full py-1 px-3 resize-none focus:outline outline-1 outline-green-500 border-b border-green-500 bg-transparent transition-all duration-300"
+                type="email"
+                placeholder="আপনার ইমেইল"
+                name="email"
+              />
+            </div>
+          </div>
+
+          {/* পাসওয়ার্ড */}
+          <div className="grid grid-cols-10">
+            <p className="col-span-2 text-end px-3">পাসওয়ার্ড:</p>
+            <div className="col-span-8">
+              <input
+                className="w-full py-1 px-3 resize-none focus:outline outline-1 outline-green-500 bg-transparent border-b border-green-500"
+                type="password"
+                placeholder="আপনার পাসওয়ার্ড"
+                name="passowrd"
+              />
+            </div>
+          </div>
+
+          <div className="text-center">
+            <button className="bg-green-500 hover:bg-green-600 transition-all duration-300 text-white py-2 px-10 mt-3 shadow-lg">
+              সাবমিট করুন
+            </button>
           </div>
         </div>
-
-        {/* পাসওয়ার্ড */}
-        <div className="grid grid-cols-10">
-          <p className="col-span-2 text-end px-3">পাসওয়ার্ড:</p>
-          <div className="col-span-8">
-            <input
-              className="w-full py-1 px-3 resize-none focus:outline outline-1 outline-green-500 bg-transparent border-b border-green-500"
-              type="password"
-              placeholder="আপনার পাসওয়ার্ড"
-              name="passowrd"
-            />
-          </div>
-        </div>
-
-        <div className="text-center">
-          <button className="bg-green-500 hover:bg-green-600 transition-all duration-300 text-white py-2 px-10 mt-3 shadow-lg">
-            সাবমিট করুন
-          </button>
-        </div>
-      </div>
-    </form>
+      </form>
+    </>
   );
 };
 
--- components/auth/signup-form.jsx
+++ components/auth/signup-form.jsx
@@ -1,82 +1,116 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 import SubTitle from "../sub-title";
 
-const SigninForm = () => {
+const SignupForm = ({ lang = "bn" }) => {
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  const handleSubmitSignup = async (event) => {
+    event.preventDefault();
+    try {
+      const formData = new FormData(event.currentTarget);
+
+      const name = formData.get("name");
+      const email = formData.get("email");
+      const number = formData.get("number");
+      const password = formData.get("password");
+
+      const response = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ name, email, number, password }),
+      });
+      response.status === 201 && router.push(`/${lang}/signin`);
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
   return (
-    <form>
-      <SubTitle showIcon={false}>সাইনআপ করুন</SubTitle>
-      <div className="flex flex-col gap-5">
-        <div className="grid grid-cols-10">
-          <p className="col-span-2 text-end px-3">নাম:</p>
-          <div className="col-span-8">
-            <input
-              className="w-full py-1 px-3 resize-none focus:outline outline-1 outline-green-500 border-b border-green-500 bg-transparent transition-all duration-300"
-              type="text"
-              placeholder="আপনার নাম"
-              name="name"
-            />
+    <>
+      {error && <div className="text-center text-sm text-red-500">{error}</div>}
+      <form onSubmit={handleSubmitSignup}>
+        <SubTitle showIcon={false}>সাইনআপ করুন</SubTitle>
+        <div className="flex flex-col gap-5">
+          <div className="grid grid-cols-10">
+            <p className="col-span-2 text-end px-3">নাম:</p>
+            <div className="col-span-8">
+              <input
+                className="w-full py-1 px-3 resize-none focus:outline outline-1 outline-green-500 border-b border-green-500 bg-transparent transition-all duration-300"
+                type="text"
+                placeholder="আপনার নাম"
+                name="name"
+              />
+            </div>
+          </div>
+
+          {/* ইমেইল */}
+          <div className="grid grid-cols-10">
+            <p className="col-span-2 text-end px-3">ইমেইল:</p>
+            <div className="col-span-8">
+              <input
+                className="w-full py-1 px-3 resize-none focus:outline outline-1 outline-green-500 border-b border-green-500 bg-transparent transition-all duration-300"
+                type="email"
+                placeholder="আপনার ইমেইল"
+                name="email"
+              />
+            </div>
+          </div>
+
+          {/* নাম্বার */}
+          <div className="grid grid-cols-10">
+            <p className="col-span-2 text-end px-3">ফোন:</p>
+            <div className="col-span-8">
+              <input
+                className="w-full py-1 px-3 resize-none focus:outline outline-1 outline-green-500 border-b border-green-500 bg-transparent transition-all duration-300"
+                type="text"
+                placeholder="আপনার ফোন নাম্বার"
+                name="number"
+              />
+            </div>
+          </div>
+
+          {/* পাসওয়ার্ড */}
+          <div className="grid grid-cols-10">
+            <p className="col-span-2 text-end px-3">পাসওয়ার্ড:</p>
+            <div className="col-span-8">
+              <input
+                className="w-full py-1 px-3 resize-none focus:outline outline-1 outline-green-500 bg-transparent border-b border-green-500"
+                type="password"
+                placeholder="পাসওয়ার্ড সেট করুন"
+                name="passowrd"
+              />
+            </div>
+          </div>
+
+          {/* কনফার্ম পাসওয়ার্ড */}
+          <div className="grid grid-cols-10">
+            <p className="col-span-2 text-end px-3">পাসওয়ার্ড:</p>
+            <div className="col-span-8">
+              <input
+                className="w-full py-1 px-3 resize-none focus:outline outline-1 outline-green-500 bg-transparent border-b border-green-500"
+                type="password"
+                placeholder="পাসওয়ার্ড কনফার্ম করুন"
+                name="conf-passowrd"
+              />
+            </div>
+          </div>
+
+          <div className="text-center">
+            <button className="bg-green-500 hover:bg-green-600 transition-all duration-300 text-white py-2 px-10 mt-3 shadow-lg">
+              সাবমিট করুন
+            </button>
           </div>
         </div>
-
-        {/* ইমেইল */}
-        <div className="grid grid-cols-10">
-          <p className="col-span-2 text-end px-3">ইমেইল:</p>
-          <div className="col-span-8">
-            <input
-              className="w-full py-1 px-3 resize-none focus:outline outline-1 outline-green-500 border-b border-green-500 bg-transparent transition-all duration-300"
-              type="email"
-              placeholder="আপনার ইমেইল"
-              name="email"
-            />
-          </div>
-        </div>
-
-        {/* নাম্বার */}
-        <div className="grid grid-cols-10">
-          <p className="col-span-2 text-end px-3">ফোন:</p>
-          <div className="col-span-8">
-            <input
-              className="w-full py-1 px-3 resize-none focus:outline outline-1 outline-green-500 border-b border-green-500 bg-transparent transition-all duration-300"
-              type="text"
-              placeholder="আপনার ফোন নাম্বার"
-              name="number"
-            />
-          </div>
-        </div>
-
-        {/* পাসওয়ার্ড */}
-        <div className="grid grid-cols-10">
-          <p className="col-span-2 text-end px-3">পাসওয়ার্ড:</p>
-          <div className="col-span-8">
-            <input
-              className="w-full py-1 px-3 resize-none focus:outline outline-1 outline-green-500 bg-transparent border-b border-green-500"
-              type="password"
-              placeholder="পাসওয়ার্ড সেট করুন"
-              name="passowrd"
-            />
-          </div>
-        </div>
-
-        {/* কনফার্ম পাসওয়ার্ড */}
-        <div className="grid grid-cols-10">
-          <p className="col-span-2 text-end px-3">পাসওয়ার্ড:</p>
-          <div className="col-span-8">
-            <input
-              className="w-full py-1 px-3 resize-none focus:outline outline-1 outline-green-500 bg-transparent border-b border-green-500"
-              type="password"
-              placeholder="পাসওয়ার্ড কনফার্ম করুন"
-              name="conf-passowrd"
-            />
-          </div>
-        </div>
-
-        <div className="text-center">
-          <button className="bg-green-500 hover:bg-green-600 transition-all duration-300 text-white py-2 px-10 mt-3 shadow-lg">
-            সাবমিট করুন
-          </button>
-        </div>
-      </div>
-    </form>
+      </form>
+    </>
   );
 };
 
-export default SigninForm;
+export default SignupForm;
