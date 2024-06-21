d88994cf75cd0bbbfa90ad564f950a7e5be8cd84
RashedAbdullah <maarashed40@gmail.com>
2024-06-21 09:42:05 +0600

    Changing students table

--- components/students/student-table.jsx
+++ components/students/student-table.jsx
@@ -3,52 +3,47 @@ import { getEngToBnNumber } from "@/utils/number-converter";
 const StudentResultTable = ({ results }) => {
   return (
     <div className="relative overflow-x-auto shadow">
-      <table className="min-w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-          <tr>
-            <th scope="col" className="px-6 py-3 text-nowrap bg-gray-200">
-              পরীক্ষা
-            </th>
-            {results[0].numbers.map((book) => (
-              <th
-                key={book.book}
-                scope="col"
-                className="px-6 py-3 text-nowrap bg-gray-200"
-              >
-                {book.book}
+      {results.map((result) => (
+        <table
+          key={result.examName}
+          className="min-w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
+        >
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3 text-nowrap bg-gray-200">
+                পরীক্ষা
               </th>
-            ))}
+              {result.numbers.map((book) => (
+                <th
+                  key={book.book}
+                  scope="col"
+                  className="px-6 py-3 text-nowrap bg-gray-200"
+                >
+                  {book.book}
+                </th>
+              ))}
 
-            <th scope="col" className="px-6 py-3 text-nowrap bg-gray-200">
-              মোট নাম্বার
-            </th>
-            <th scope="col" className="px-6 py-3 text-nowrap bg-gray-200">
-              গড় নাম্বার
-            </th>
-            <th scope="col" className="px-6 py-3 text-nowrap bg-gray-200">
-              মান
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {results.map((result) => (
-            <tr
-              key={result.examName}
-              className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
-            >
+              <th scope="col" className="px-6 py-3 text-nowrap bg-gray-200">
+                মোট নাম্বার
+              </th>
+              <th scope="col" className="px-6 py-3 text-nowrap bg-gray-200">
+                গড় নাম্বার
+              </th>
+              <th scope="col" className="px-6 py-3 text-nowrap bg-gray-200">
+                মান
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
               <th
                 scope="row"
                 className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white bg-gray-200 border-t border-gray-400"
               >
-                {result.examName}
+                প্রথম সাময়ীক পরীক্ষা
               </th>
               {result.numbers.map((number) => (
-                <td
-                  key={number.book}
-                  className={`px-6 py-4 ${
-                    number.number < 35 && "text-red-500"
-                  }`}
-                >
+                <td key={number.number} className={`px-6 py-4 `}>
                   {getEngToBnNumber(number.number)}
                 </td>
               ))}
@@ -57,9 +52,9 @@ const StudentResultTable = ({ results }) => {
               <td className="px-6 py-4">৭৬.৭</td>
               <td className="px-6 py-4">মুমতাজ</td>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </tbody>
+        </table>
+      ))}
     </div>
   );
 };
