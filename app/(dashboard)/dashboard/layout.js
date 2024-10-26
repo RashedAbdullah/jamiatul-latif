be18d6e65321fcb60491fc5bf7d25e2709c8700e
RashedAbdullah <maarashed40@gmail.com>
2024-10-26 20:46:17 +0600

    Rashed Abdullah

--- app/(dashboard)/dashboard/layout.js
+++ app/(dashboard)/dashboard/layout.js
@@ -6,7 +6,7 @@ import { redirect } from "next/navigation";
 const DashboardLayout = async ({ children }) => {
   const session = await auth();
   if (!session) {
-    redirect("/");
+    redirect("/signin");
   }
 
   return (
